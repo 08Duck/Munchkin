e64d5ff2efe01f7a52bbc0ce26a8b972022c4901
08Duck <edvin@pinnarpsgard.se>
2026-03-23 13:35:28 +0100

    classes

---
+++ Munchkin/Card.cs
@@ -0,0 +1,37 @@
+﻿using Munchkin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Munchkin
+{
+    abstract class Card
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Card(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        // Alla kort kan visa sig själva (ASCII)
+        public virtual void Display()
+        {
+            Console.WriteLine("+----------------------+");
+            Console.WriteLine($"| {Name.PadRight(20)} |");
+            Console.WriteLine("|----------------------|");
+            Console.WriteLine($"| {Description.PadRight(20)} |");
+            Console.WriteLine("+----------------------+");
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Munchkin/TreasureCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Munchkin
+{
+    abstract class TreasureCard : Card
+    {
+        public TreasureCard(string name, string desc) : base(name, desc) { }
+    }
+}
