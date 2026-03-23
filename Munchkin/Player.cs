15c790145562f9b8153487df199305ba212a3d01
WI3R <william.betnerrosen@elev.ga.lbs.se>
2026-03-23 14:34:48 +0100

    player update

--- Munchkin/Player.cs
+++ Munchkin/Player.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Munchkin
 {
-    internal class Player
+    class Player
     {
+        public string Name { get; set; }
+        public int Level { get; set; }
+
+        public List<Card> Hand { get; set; }
+        public List<Item> Equipment { get; set; }
+
+        public Player(string name)
+        {
+            Name = name
+            Level = 1;
+
+            Hand = new List<Card>();
+            Equipment = new List<Item>();
+        }
+
+        public void AddCardToHand(Card card)
+        {
+            Hand.Add(card);
+        }
+
+        public void EquipItem(Item item)
+        {
+            if (Hand.Contains(item))
+            {
+                Equipment.Add(item);
+                Hand.Remove(item);
+            }
+        }
+
+
     }
 }
